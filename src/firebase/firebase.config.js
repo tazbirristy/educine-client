0e0d93346e6bcf72d32ae71003c663791935c831
tazbirristy <tazbirristyweb@gmail.com>
2022-10-25 00:38:47 +0600

    initializing project with installation

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyACw5RLrS60qQUDUz9s70599V6p6Nlr3IQ",
+  authDomain: "educine-9f86e.firebaseapp.com",
+  projectId: "educine-9f86e",
+  storageBucket: "educine-9f86e.appspot.com",
+  messagingSenderId: "857196763584",
+  appId: "1:857196763584:web:0f037caafbe6f6b1f7d025",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
