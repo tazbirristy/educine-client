590236c6cab91db32dd2b06ac5c2606d90c6008a
tazbirristy <tazbirristyweb@gmail.com>
2022-10-26 06:00:15 +0600

    env.local completed

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,12 +1,12 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyACw5RLrS60qQUDUz9s70599V6p6Nlr3IQ",
-  authDomain: "educine-9f86e.firebaseapp.com",
-  projectId: "educine-9f86e",
-  storageBucket: "educine-9f86e.appspot.com",
-  messagingSenderId: "857196763584",
-  appId: "1:857196763584:web:0f037caafbe6f6b1f7d025",
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
 };
 
 const app = initializeApp(firebaseConfig);
