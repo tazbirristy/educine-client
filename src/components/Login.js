8ba056413f4d870fe6e9e92329bed9e347118989
tazbirristy <tazbirristyweb@gmail.com>
2022-10-27 04:14:37 +0600

    login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,53 @@
 import React from "react";
+import { MDBInput, MDBCol, MDBRow, MDBBtn, MDBIcon } from "mdb-react-ui-kit";
+import { Link } from "react-router-dom";
+import { Button } from "react-bootstrap";
 
 const Login = () => {
   return (
-    <div>
-      <h1>Login</h1>
+    <div className="w-50 justify-content-center mx-auto">
+      <h1 className="text-center text-primary mt-3">Please LogIn!!!</h1>
+      <form className="m-3">
+        <MDBInput
+          className="mb-4"
+          type="email"
+          id="form2Example1"
+          label="Email address"
+          placeholder="Enter Your Email"
+        />
+        <MDBInput
+          className="mb-4"
+          type="password"
+          id="form2Example2"
+          label="Password"
+          placeholder="Enter Your Password"
+        />
+
+        <MDBRow className="mb-4">
+          <MDBCol>
+            <a href="#!">Forgot password?</a>
+          </MDBCol>
+        </MDBRow>
+
+        <Button type="submit" className="mb-4 w-100" block>
+          Sign in
+        </Button>
+
+        <div className="text-center">
+          <p>
+            Not a member? <Link to="/register">Register</Link>
+          </p>
+          <p>or SignIn with:</p>
+
+          <Button floating className="mx-1">
+            <MDBIcon fab icon="google" />
+          </Button>
+
+          <Button floating className="mx-1">
+            <MDBIcon fab icon="github" />
+          </Button>
+        </div>
+      </form>
     </div>
   );
 };
