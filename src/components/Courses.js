a699e7e7954413796cf1d700de0d239ebfc709fa
tazbirristy <tazbirristyweb@gmail.com>
2022-10-26 07:20:57 +0600

    header completed

--- src/components/Courses.js
+++ src/components/Courses.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Courses = () => {
-  return <div></div>;
+  return (
+    <div>
+      <h1>This is courses</h1>
+    </div>
+  );
 };
 
 export default Courses;
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,17 +2,35 @@ import React from "react";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <Navbar bg="light" expand="lg">
+    <Navbar bg="primary" expand="lg">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand>
+          <Link to="/" className="text-white text-decoration-none fw-bolder">
+            Educine
+          </Link>
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
+            <Link className="text-white me-3 text-decoration-none" to="/">
+              Home
+            </Link>
+            <Link
+              className="text-white me-3 text-decoration-none"
+              to="/courses"
+            >
+              Courses
+            </Link>
+            <Link className="text-white me-3 text-decoration-none" to="/blog">
+              Blog
+            </Link>
+            <Link className="text-white me-3 text-decoration-none" to="/login">
+              Login
+            </Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
